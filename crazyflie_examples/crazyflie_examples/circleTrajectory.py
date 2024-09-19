6dcc661f187899343f030a4ed38f9003b11c1f51
valiokei <36767334+valiokei@users.noreply.github.com>
2024-09-19 16:49:56 +0200

    Added new Variable to log. Improved the landing removing useless gotTo positions

--- crazyflie_examples/crazyflie_examples/circleTrajectory.py
+++ crazyflie_examples/crazyflie_examples/circleTrajectory.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+
+#  usbipd attach -a -i 1915:7777 -w
+
 from pathlib import Path
 
 import numpy as np
@@ -14,16 +17,17 @@ def main():
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
 
-    traj1 = Trajectory()
+    # traj1 = Trajectory()
     # traj1.loadcsv(Path(__file__).parent / 'data/circle.csv')
     
-    traj1.loadcsv('/home/valiokei/Documenti/GitHub/ros2_ws/src/crazyswarm2/crazyflie_examples/crazyflie_examples/data/square.csv')
+    # traj1.loadcsv('/home/valiokei/Documenti/GitHub/ros2_ws/src/crazyswarm2/crazyflie_examples/crazyflie_examples/data/square.csv')
     
     # enable logging
     allcfs.setParam('usd.logging', 1)
 
 
-    HEIGHT = 0.30
+    HEIGHT = 0.15
+    DISTANCE = 0.40
 
     TRIALS = 1
     TIMESCALE = 1.0
@@ -34,66 +38,67 @@ def main():
         #     print('Battery is low, landing and aborting')
         #     exit(1)
 
-
-        for cf in allcfs.crazyflies:
-            cf.uploadTrajectory(0, 0, traj1)
+        cf = allcfs.crazyflies[0]
+        # for cf in allcfs.crazyflies:
+        #     cf.uploadTrajectory(0, 0, traj1)
 
         allcfs.takeoff(targetHeight=HEIGHT, duration=2.0)
-        timeHelper.sleep(3.0)
+        timeHelper.sleep(2.0)
         
-
         # allcfs.startTrajectory(0, timescale=TIMESCALE)
         # timeHelper.sleep(traj1.duration * TIMESCALE+2.0)
        
         # allcfs.startTrajectory(0, timescale=TIMESCALE, reverse=True)
         # timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
 
+        # GO TO CENTER
+        pos =  np.array([0.0, 0.0, HEIGHT])
+        cf.goTo(pos, 0, 1.0)
+        timeHelper.sleep(1.0)
+
+        
+
+
+        # start_time = time.time()
+        # while time.time() - start_time < 4:
+        #     # GO TO CENTER
+        #     pos =  np.array([0.0, 0.00, HEIGHT])
+        #     cf.goTo(pos, 0, 0.5)
+        #     timeHelper.sleep(0.5)
+
+
+        # GO out
+        pos =  np.array([0.0, -DISTANCE, HEIGHT])
+        cf.goTo(pos, 0, 3.0)
+        timeHelper.sleep(3.0)
+
+        # stay out
+        pos =  np.array([0.0, -DISTANCE, HEIGHT])
+        cf.goTo(pos, 0, 1.0)
+        timeHelper.sleep(1.0)
+
+        # stay out
+        pos =  np.array([0.0, -DISTANCE, HEIGHT])
+        cf.goTo(pos, 0, 1.0)
+        timeHelper.sleep(1.0)
+
+        # stay out
+        pos =  np.array([0.0, -DISTANCE, HEIGHT])
+        cf.goTo(pos, 0, 1.0)
+        timeHelper.sleep(1.0)
 
         # GO TO CENTER
         pos =  np.array([0.0, 0.0, HEIGHT])
-        cf.goTo(pos, 0, 2.0)
-        timeHelper.sleep(2.5)
-
-
-        start_time = time.time()
-        while time.time() - start_time < 10:
-            # GO TO CENTER
-            pos =  np.array([0.0, 0.00, HEIGHT])
-            cf.goTo(pos, 0, 0.5)
-            timeHelper.sleep(0.5)
-
-
-        # # GO TO CENTER
-        # pos =  np.array([0.0, 0.20, HEIGHT])
-        # cf.goTo(pos, 0, 2.0)
-        # timeHelper.sleep(5)
-
-        # # GO TO CENTER
-        # pos =  np.array([0.0, 0.0, HEIGHT])
-        # cf.goTo(pos, 0, 2.0)
-        # timeHelper.sleep(2.5)
-
-        # # GO TO CENTER
-        # pos =  np.array([0.0, 0.0, HEIGHT])
-        # cf.goTo(pos, 0, 1.0)
-        # timeHelper.sleep(2.0)
-        # pos =  np.array([0.0, 0.0, HEIGHT])
-        # cf.goTo(pos, 0, 1.0)
-        # timeHelper.sleep(2.0)
-
-
-        # GO TO land
-        pos =  np.array([0.0, 0.0, HEIGHT/2])
+        cf.goTo(pos, 0, 3.0)
+        timeHelper.sleep(3.0)
+         
+        # GO TO CENTER
+        pos =  np.array([0.0, 0.0, HEIGHT])
         cf.goTo(pos, 0, 1.0)
-        timeHelper.sleep(1.5)
+        timeHelper.sleep(1.0)
 
-        # GO TO land
-        pos =  np.array([0.0, 0.0, HEIGHT/4])
-        cf.goTo(pos, 0, 1.0)
-        timeHelper.sleep(1.5)
 
-        # TO TRY: provare a fare atterrare il drone usando direttamente la traiettoria fino a terra
-        allcfs.land(targetHeight=0.08, duration=1.0)
+        allcfs.land(targetHeight=0.05, duration=2.0)
         timeHelper.sleep(2.0)
 
     # disable logging
